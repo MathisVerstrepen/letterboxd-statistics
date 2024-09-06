0ea868cabae6f3cafe9ab483ad587260842e2d49
Mathis Verstrepen <mathisverstrepen@gmail.com>
2024-09-05 22:41:12 -0400

    🎨 Improve chart visuals

--- app-client/assets/graphs/createGraph.js
+++ app-client/assets/graphs/createGraph.js
@@ -1,37 +1,111 @@
 window.createChart = function (pointsRawData) {
+    // Parse the raw data into an array of objects.
     aapl = pointsRawData.split(";").map(function (point) {
         var xy = point.split(":").map(Number);
         return { date: new Date(xy[0]), value: xy[1] };
     });
 
     // Declare the chart dimensions and margins.
-    const width = 928;
-    const height = 500;
-    const marginTop = 20;
-    const marginRight = 30;
-    const marginBottom = 30;
-    const marginLeft = 40;
+    var margin = { top: 10, right: 20, bottom: 50, left: 60 },
+        width = 1200 - margin.left - margin.right,
+        height = 800 - margin.top - margin.bottom;
+
+    // Append the SVG element to the container.
+    const svg = d3
+        .select("#container")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    // Define area and line gradient.
+    var lg = svg
+        .append("defs")
+        .append("linearGradient")
+        .attr("id", "chartGradient")
+        .attr("x1", "0%")
+        .attr("x2", "0%")
+        .attr("y1", "0%")
+        .attr("y2", "100%"); //vertical linear gradient
+
+    lg.append("stop")
+        .attr("offset", "0%")
+        .style("stop-color", "#FF8000")
+        .style("stop-opacity", 1);
+
+    lg.append("stop")
+        .attr("offset", "100%")
+        .style("stop-color", "#40BCF4")
+        .style("stop-opacity", 1);
 
     // Declare the x (horizontal position) scale.
     const x = d3.scaleUtc(
+        // d3.extent returns the minimum and maximum value in the given array using natural order.
         d3.extent(aapl, function (d) {
             return d.date;
         }),
-        [marginLeft, width - marginRight]
+        [0, width]
     );
 
+    // Add the x-axis.
+    svg.append("g")
+        .attr("class", "axisWhite")
+        .attr("transform", "translate(0," + (height + 10) + ")")
+        .call(
+            d3
+                .axisBottom(x)
+                .ticks(width / 100)
+                .tickSizeOuter(0)
+        )
+        .call(function (g) {
+            return g.select(".domain").remove();
+        });
+
+    ymin = d3.min(aapl, function (d) {
+        return d.value;
+    });
+    ymax = d3.max(aapl, function (d) {
+        return d.value;
+    });
+
     // Declare the y (vertical position) scale.
-    const y = d3.scaleLinear(
-        [
-            d3.min(aapl, function (d) {
-                return d.value;
-            }),
-            d3.max(aapl, function (d) {
-                return d.value;
-            }),
-        ],
-        [height - marginBottom, marginTop]
-    );
+    const y = d3.scaleLinear([ymin, ymax], [height, 0]);
+
+    // Add the y-axis, remove the domain line, add grid lines.
+    svg.append("g")
+        .attr("class", "axisWhite")
+        .attr("transform", "translate(-5,0)")
+        .call(d3.axisLeft(y).ticks(height / 80))
+        .call(function (g) {
+            return g.select(".domain").remove();
+        })
+        // Add grid lines.
+        .call(function (g) {
+            return g
+                .selectAll(".tick line")
+                .clone()
+                .attr("x2", width)
+                .attr("stroke-opacity", 0.1);
+        });
+
+    // Declare the area generator.
+    const area = d3
+        .area()
+        .x(function (d) {
+            return x(d.date);
+        })
+        .y0(height)
+        .y1(function (d) {
+            return y(d.value);
+        });
+
+    // Append a path for the area.
+    svg.append("path")
+        .attr("fill", "url(#chartGradient)")
+        .attr("fill-opacity", 0.2)
+        .attr("stroke", "none")
+        .attr("d", area(aapl));
 
     // Declare the line generator.
     const line = d3
@@ -43,55 +117,10 @@ window.createChart = function (pointsRawData) {
             return y(d.value);
         });
 
-    // Create the SVG container.
-    const svg = d3
-        .create("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .attr("viewBox", [0, 0, width, height])
-        .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
-
-    // Add the x-axis.
-    svg.append("g")
-        .attr("transform", "translate(0," + height - marginBottom + ")")
-        .call(
-            d3
-                .axisBottom(x)
-                .ticks(width / 80)
-                .tickSizeOuter(0)
-        );
-
-    // Add the y-axis, remove the domain line, add grid lines and a label.
-    svg.append("g")
-        .attr("transform", "translate(" + marginLeft + ",0)")
-        .call(d3.axisLeft(y).ticks(height / 40))
-        .call(function (g) {
-            return g.select(".domain").remove();
-        })
-        .call(function (g) {
-            return g
-                .selectAll(".tick line")
-                .clone()
-                .attr("x2", width - marginLeft - marginRight)
-                .attr("stroke-opacity", 0.1);
-        })
-        .call(function (g) {
-            return g
-                .append("text")
-                .attr("x", -marginLeft)
-                .attr("y", 10)
-                .attr("fill", "currentColor")
-                .attr("text-anchor", "start")
-                .text("Watchcount");
-        });
-
     // Append a path for the line.
     svg.append("path")
         .attr("fill", "none")
-        .attr("stroke", "steelblue")
-        .attr("stroke-width", 1.5)
+        .attr("stroke", "url(#chartGradient)")
+        .attr("stroke-width", 3)
         .attr("d", line(aapl));
-
-    // Append the SVG element.
-    document.getElementById("container").appendChild(svg.node());
 };
